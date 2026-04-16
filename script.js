dc8a6558b6fc5fa59d2889c6c08f6335f5615837
jairtonjunior19-ops <jairtonjunior19@gmail.com>
2026-04-16 20:28:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+class Aluno {
+    constructor(id, nome, turma) {
+        this.id = id;
+        this.nome = nome;
+        this.turma = turma;
+    }
+
+    // getters e setters
+}
+
+class Presenca {
+    constructor(idAluno, data, presente) {
+        this.idAluno = idAluno;
+        this.data = data;
+        this.presente = presente;
+    }
+
+    // getters e setters
+}
